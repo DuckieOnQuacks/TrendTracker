d0b4a529eb1c0126a260fbc41b51265c0beef3d8
DuckieOnQuacks <jojopaschke@gmail.com>
2024-04-15 10:19:41 -0700

    Added more data

--- tiktokScraper.py
+++ tiktokScraper.py
@@ -6,42 +6,46 @@ from datetime import datetime
 # Initialize a set to hold processed video IDs
 processedVideoIds = set()
 
-# Read existing video IDs from the file into the set to check for duplicates
-with open('TrendTracker\TiktokData.json', 'r', encoding='utf-8') as f:
-    for line in f:
-        videoId = line.split(',')[0] 
-        processedVideoIds.add(videoId) #.add doesnt do anything if item is already in the set
-        print(videoId)
+def scrapeData():
+    # Read existing video IDs from the file into the set to check for duplicates
+    with open('TiktokData.json', 'r', encoding='utf-8') as f:
+        for line in f:
+            videoId = line.split(',')[0] 
+            processedVideoIds.add(videoId) #.add doesnt do anything if item is already in the set
+            print(videoId)
 
-# Prepare the output file for appending new data
-outputFile = open('TrendTracker\TiktokData.json', 'a', encoding='utf-8')
-#Connect to api
-url = "https://tiktok-api23.p.rapidapi.com/api/post/trending"
-headers = {
-    "X-RapidAPI-Key": "b914b9089amsh57d0a4103d72072p1b9c18jsna3a7dd730f95",
-    "X-RapidAPI-Host": "tiktok-api23.p.rapidapi.com"
-}
+    # Prepare the output file for appending new data
+    outputFile = open('TiktokData.json', 'a', encoding='utf-8')
+    #Connect to api
+    url = "https://tiktok-api23.p.rapidapi.com/api/post/trending"
+    headers = {
+        "X-RapidAPI-Key": "b914b9089amsh57d0a4103d72072p1b9c18jsna3a7dd730f95",
+        "X-RapidAPI-Host": "tiktok-api23.p.rapidapi.com"
+    }
 
-querystring = {"count": "30"}
-response = requests.get(url, headers=headers, params=querystring)
-jsonData = response.json()
-lineToWrite = {}
+    querystring = {"count": "30"}
+    response = requests.get(url, headers=headers, params=querystring)
+    jsonData = response.json()
+    lineToWrite = {}
 
-for item in jsonData["itemList"]:
-    videoId = item["id"]
-    if videoId not in processedVideoIds: # Check to see if the post ID isnt already in the file.
-        hashtags = re.findall(r'#\w+', item['desc']) # Extract hastags from title.
-        if all(re.match(r'^#[A-Za-z0-9_]+$', tag) for tag in hashtags): # Regex to check if hashtag is english
-            videoDetails = {
-                "videoId": videoId,
-                "likes": item["stats"]["diggCount"],
-                "followerCount": item["authorStats"]["followerCount"],
-                "hashtags": hashtags,
-                "createDate": datetime.fromtimestamp(item["createTime"]).strftime('%Y-%m-%d %H:%M:%S')
-            }
-            outputFile.write(json.dumps(videoDetails) + '\n') # Convert the dictionary to a JSON string and write it to the file
+    for item in jsonData["itemList"]:
+        videoId = item["id"]
+        if videoId not in processedVideoIds: # Check to see if the post ID isnt already in the file.
+            hashtags = re.findall(r'#\w+', item['desc']) # Extract hastags from title.
+            if all(re.match(r'^#[A-Za-z0-9_]+$', tag) for tag in hashtags): # Regex to check if hashtag is english
+                videoDetails = {
+                    "videoId": videoId,
+                    "likes": item["stats"]["diggCount"],
+                    "followerCount": item["authorStats"]["followerCount"],
+                    "hashtags": hashtags,
+                    "createDate": datetime.fromtimestamp(item["createTime"]).strftime('%Y-%m-%d %H:%M:%S')
+                }
+                outputFile.write(json.dumps(videoDetails) + '\n') # Convert the dictionary to a JSON string and write it to the file
 
-# Close the file after writing
-outputFile.close()
+    # Close the file after writing
+    outputFile.close()
+    print("Data extraction and saving process completed.")
 
-print("Data extraction and saving process completed.")
+
+for i in range(0,8):
+    scrapeData()
